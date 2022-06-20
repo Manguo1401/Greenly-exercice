af2d8051caacdbd7bd777857115cbbeb18fd42d5
ahartnell <ahartnell@groupcls.com>
2022-06-20 15:35:33 +0200

    types added to store and index

---
+++ README.solution.md
@@ -0,0 +1,18 @@
+# Steps for the exercise 
+
+## Intall Typescript for types
+
+- Install typescript to ensure type security 
+
+## Create all jest unit tests
+
+- Check with Jest that all the cases given in the exercise are ok and secure code modification when the main method will be refactored
+
+## Refactor method
+
+- The store class is a if/else mess. It needs to be split into comprehensible small methods or functions (Keep It Simple Stupid)
+
+## Add new tests and comply to them by integrating the new code
+
+- Add new tests to prepare for the evolutions asked by the exercise
+- Add the evolutions in the code
--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
-import { Store, DiscountOffer } from "./store";
-
-import fs from "fs";
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const store_1 = require("./store");
+const fs_1 = __importDefault(require("fs"));
 const discountOffers = [
-  new DiscountOffer("Velib", 20, 30),
-  new DiscountOffer("Naturalia", 10, 5),
-  new DiscountOffer("Vinted", 5, 40),
-  new DiscountOffer("Ilek", 15, 40)
+    new store_1.DiscountOffer("Velib", 20, 30),
+    new store_1.DiscountOffer("Naturalia", 10, 5),
+    new store_1.DiscountOffer("Vinted", 5, 40),
+    new store_1.DiscountOffer("Ilek", 15, 40)
 ];
-const store = new Store(discountOffers);
-
+const store = new store_1.Store(discountOffers);
 const log = [];
-
 for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
-  log.push(JSON.stringify(store.updateDiscounts()));
+    log.push(JSON.stringify(store.updateDiscounts()));
 }
-
 /* eslint-disable no-console */
-fs.writeFile("output.txt", log.toString(), err => {
-  if (err) {
-    console.log("error");
-  } else {
-    console.log("success");
-  }
+fs_1.default.writeFile("output.txt", log.toString(), err => {
+    if (err) {
+        console.log("error");
+    }
+    else {
+        console.log("success");
+    }
 });
 /* eslint-enable no-console */
